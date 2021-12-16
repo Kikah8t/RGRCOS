c8f2a0f14509eed386475a2d41c4c36ce3241612
Kikah8t <91501333+Kikah8t@users.noreply.github.com>
2021-12-16 21:44:38 +0300

    Update RGR_Zad1.m

--- RGR_Zad1.m
+++ RGR_Zad1.m
@@ -1,3 +1,4 @@
+%1.4
 Fd = 1e5; %Частота дискретизации
 Td = 1/Fd; %Период Дискретизации
 N = 1e5; %Кол-во отсчетов сигнала
@@ -29,10 +30,10 @@ C2(1) = (1/Tn2)*abs(X2(1));
 
 % Построение графиков
 figure('Name','Вариант 20','NumberTitle','off');
-subplot(2,2,1),stem(t*N,x1,'.'),grid on,xlim([0 2*N*T1]),title('Сигнал x1[n]'),xlabel('n'),ylabel('x1[n] [В * c]');
-subplot(2,2,3),stem(t*N,x2,'.'),grid on,xlim([0 2*N*T2]),title('Сигнал x2[n]'),xlabel('n'),ylabel('x2[n] [В * c]');
-subplot(2,2,2),stem(f/F1,C1),grid on,xlim([0 10]),ylim([0 3.5e-4]),title('Спектр X1[m]'),xlabel('m'),ylabel('X1[m] [В]');
-subplot(2,2,4),stem(f/F2,C2),grid on,xlim([0 10]),title('Спектр X2[m]'),xlabel('m'),ylabel('X2[m] [В]');
+subplot(2,2,1),stem(t,x1,'.'),grid on,xlim([0 2*T1]),title('Сигнал x1[n]'),xlabel('n*Td'),ylabel('x1[n] [В * c]');
+subplot(2,2,3),stem(t,x2,'.'),grid on,xlim([0 2*T2]),title('Сигнал x2[n]'),xlabel('n*Td'),ylabel('x2[n] [В * c]');
+subplot(2,2,2),stem(f,C1),grid on,xlim([0 10*F1]),ylim([0 3.5e-4]),title('Спектр X1[m]'),xlabel('m/T1 [Гц]'),ylabel('X1[m] [В]');
+subplot(2,2,4),stem(f,C2),grid on,xlim([0 10*F2]),title('Спектр X2[m]'),xlabel('m/T2 [Гц]'),ylabel('X2[m] [В]');
 
 figure('Name','Вариант 20','NumberTitle','off');
 subplot(2,2,1),stem(f/F1,C1),grid on,xlim([1980 Tn1]),ylim([0 3.5e-4]),title('Спектр X1[m]'),xlabel('m'),ylabel('X1[m] [В]');
